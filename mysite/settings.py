ea4283f498157c2ac9d5fb276713e83ffe03049b
Kim2507 <mykim250795@gmail.com>
2021-12-05 13:09:35 -0500

    adding changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+# Proceed to alter the DATABASES dictionary
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_db',
+        'USER': 'app-user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
