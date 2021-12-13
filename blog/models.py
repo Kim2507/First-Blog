76b7cac9b3d06e4c8ea27653f1dae05c7a7f677a
Kim2507 <mykim250795@gmail.com>
2021-12-13 11:46:18 -0500

    adding changes

--- blog/models.py
+++ blog/models.py
@@ -20,5 +20,13 @@ class Post(models.Model):
 class Books(models.Model):
     ISBN = models.CharField(max_length=200)
     title= models.CharField(max_length=200)
-    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,max_length=200)
+    genre = models.CharField(max_length=200,default="")
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     published_date= models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from blog.views import PostViewSet
+from blog.views import PostViewSet,BooksViewSet
 
 router = routers.DefaultRouter()
 router.register(r'post', PostViewSet)
-
+router.register(r'books',BooksViewSet)
 
 urlpatterns = [
   path('', include(router.urls)),
   path('', views.post_list, name='post_list'),
+  path('',views.books_list,name='books_list'),
+  
 ]
